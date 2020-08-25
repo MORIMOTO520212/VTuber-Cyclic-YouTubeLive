a748209cc78cf2ef575ef2d3412884a52004c8c2
めだか <kanten520212@gmail.com>
2020-08-25 18:44:43 +0900

    2020/08/25

--- streamingSearch.py
+++ streamingSearch.py
@@ -6,3 +6,22 @@
 # 100件取得できるので0~99まで回す。取得順序はランダムである。
 # document.querySelectorAll("#details")[0].getElementsByTagName("span")[2].innerText;
 # "ライブ配信中"
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+# ユーザープロファイルを使う
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=options)
+driver.get("http://free-proxy.cz/ja/proxylist/main/1")
+source = driver.page_source
+driver.quit()
+
+soup = BeautifulSoup(res.text, 'html.parser')
+
+
+details = soup.find_all("div", id="details")
+
+print(details)
