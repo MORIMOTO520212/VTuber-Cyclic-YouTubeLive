d4fb13bcb8d55c21b6f79f0418e4628374dc99e5
めだか <kanten520212@gmail.com>
2020-08-28 16:33:12 +0900

    2020/08/28

--- streamingSearch.py
+++ streamingSearch.py
@@ -10,19 +10,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from time import sleep
 
 # ヘッドレスモードでユーザープロファイルを使う 
 PROFILE_PATH = "C:\\Users\\kante\\AppData\\Local\\Google\\Chrome\\User Data"
 options = Options()
-# options.add_argument("--headless")
+# ヘッドレスではプロファイル情報を読み込まない
+#options.add_argument("--headless")
+#options.add_argument("--remote-debugging-port=9222")
 options.add_argument('--user-data-dir=' + PROFILE_PATH)
-# options.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(chrome_options=options)
-driver.get("https://www.youtube.com/feed/subscriptions")
-source = driver.page_source
 
-soup = BeautifulSoup(source, 'html.parser')
-details = soup.find_all("div", id="details")
+print("ライブ配信サーチ\n\n5分ごとに更新します。終了するにはCtril + Cを押してください。")
+while True:
+    try:
+        driver.get("https://www.youtube.com/feed/subscriptions")
+        source = driver.page_source
 
-input(len(details))
-
+        soup = BeautifulSoup(source, 'html.parser')
+        details = soup.find_all("div", id="details")
+        print(len(details))
+        for detail in details:
+            channelLink = detail.find("a", class_=["yt-simple-endpoint style-scope", "yt-formatted-string"]).get("href")
+            channelId = channelLink.replace("/channel/", "")
+            # document.querySelectorAll("#details")[0].getElementsByTagName("span")[2].innerText;
+            print(channelId)
+            streamingNow = detail.find("span").get_text() # ~人が視聴中
+            print(streamingNow)
+        
+        sleep(180)
+    except KeyboardInterrupt:
+        print("キーが押されたので終了します。")
+        driver.quit()
