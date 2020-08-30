25657f106eb79d6d9b7dae8b5c29ef4cdd69acc9
MORIMOTO520212 <kanten520212@gmail.com>
2020-08-30 17:23:26 +0900

    2020/08/30

--- streamingSearch.py
+++ streamingSearch.py
@@ -36,18 +36,24 @@ while True:
         print(len(details))
         for detail in details:
             channelLink = detail.find("a", class_=["yt-simple-endpoint style-scope", "yt-formatted-string"]).get("href")
-            channelId = channelLink.replace("/channel/", "")
-            # document.querySelectorAll("#details")[0].getElementsByTagName("span")[2].innerText;
-            
+            channelId   = channelLink.replace("/channel/", "") # チャンネルID
             try:
-                streamingNow = detail.find_all("span", class_=["ytd-badge-supported-renderer"])
+                streamingNow    = detail.find_all("span", class_=["ytd-badge-supported-renderer"])
+                streamingNumber = detail.find_all("span", class_=["ytd-grid-video-renderer"])
                 streamingNow = streamingNow[len(streamingNow)-1].get_text() # ライブ配信中
-            except:
+                print(streamingNumber)
+                streamingNumber = streamingNumber[len(streamingNumber)-1].replace(" 人が視聴中", "") # 同時接続者数
+            except Exception as e:
                 streamingNow = False # 取得できなかった場合
 
             if streamingNow == "ライブ配信中":
-                streamingChannels.append(channelId)
+                print("配信中："+channelId)
+                streamingChannels.append({"channelId": channelId, "streamingNumber": streamingNumber})
         
+        if streamingChannels == []: # ライブ配信を誰もしていない場合は今後の予定を記録する
+            print("ライブ配信者なし")
+
+
         with open("assets/streaming.json", "w") as f:
             json.dump(streamingChannels, f, indent=4)
         
