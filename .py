40a7ab16c4d557623fe879bef890b1878ff972f6
MORIMOTO520212 <kanten520212@gmail.com>
2020-09-19 21:45:17 +0900

    2020/09/19

--- .py
+++ .py
@@ -1,6 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
-res = requests.get("http://twttrend.com", headers=headers)
-soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
+import pings
