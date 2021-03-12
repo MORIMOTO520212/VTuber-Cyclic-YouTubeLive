85f3475279f11525372f230f56c7423fd31629d4
MORIMOTO520212 <kanten520212@gmail.com>
2021-03-12 14:25:56 +0900

    2021.03.12

--- mining/assets/statusWindow.js
+++ mining/assets/statusWindow.js
@@ -1,3 +1,4 @@
+var ctx = document.getElementById("myChart");
 var element_youtube = document.getElementById("youtube");
 var element_statusWindow = document.getElementById("jquery-ui-draggable");
 var element_mask = document.getElementById("mask");
@@ -10,6 +11,7 @@ var element_streamingNumber = document.getElementById("streamingNumber");
 var element_livePoint = document.getElementById("livePoint");
 var element_twitterId_link = document.getElementById("twitterId_link");
 var element_youtubeId_link = document.getElementById("youtubeId_link");
+var element_playgame = document.getElementById("playgame");
 var element_playgame_link = document.getElementById("playgame_link");
 var element_playgame_photo = document.getElementById("playgame_photo");
 
@@ -18,6 +20,23 @@ jQuery(function() { // 要素ドラッグ移動
     jQuery('#jquery-ui-draggable').draggable({
         handle: 'div'
     });
+});
+
+var chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [
+            "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", 
+            "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"
+        ],
+        datasets: [{
+            label: 'ライブポイント',
+            data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            backgroundColor: 'rgba(255, 98, 132, 0.5)',
+            borderColor: 'rgba(255, 56, 99, 1)',
+            borderWidth: 1
+        }]
+    }
 });
 
 function status(channelId){
@@ -38,10 +57,26 @@ function status(channelId){
             element_videoTitle.innerText = streamings[i]["videoTitle"];
             element_streamingNumber.innerText = streamings[i]["streamingNumber"];
             element_livePoint.innerText = streamings[i]["livePoint"];
-            element_playgame_link.setAttribute("href", streamings[i]["play"]["url"]);
-            element_playgame_photo.setAttribute("src", streamings[i]["play"]["photo"]);
+            if(streamings[i]["play"]){
+                element_playgame.setAttribute("style", "display:block;width:100%;"); // playgame block
+                element_playgame_link.setAttribute("href", streamings[i]["play"]["url"]);
+                element_playgame_photo.setAttribute("src", streamings[i]["play"]["photo"]);
+            }else{
+                element_playgame.setAttribute("style", "display:none;width:100%;"); // playgame hidden
+            }
             element_twitterId_link.setAttribute("href", "https://twitter.com/"+streamings[i]["twitterId"]);
             element_youtubeId_link.setAttribute("href", "https://www.youtube.com/watch?v="+videoId);
+            // active chart
+            var activeStatus = streamings[i]["livePointStatus"];
+            chart.data.datasets[0].data =   [
+                activeStatus["00"], activeStatus["01"], activeStatus["02"], activeStatus["03"], 
+                activeStatus["04"], activeStatus["05"], activeStatus["06"], activeStatus["07"],
+                activeStatus["08"], activeStatus["09"], activeStatus["10"], activeStatus["11"], 
+                activeStatus["12"], activeStatus["13"], activeStatus["14"], activeStatus["15"], 
+                activeStatus["16"], activeStatus["17"], activeStatus["18"], activeStatus["19"], 
+                activeStatus["20"], activeStatus["21"], activeStatus["22"], activeStatus["23"]
+            ];
+            chart.update();
         }
     }
     let domain = document.domain;
