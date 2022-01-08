3430ecbb1f6ae4b169f88a4d0e9e3a5d7b5f558b
MorimotoYuma <kanten520212@gmail.com>
2022-01-08 22:35:51 +0900

    2022.01.08

--- statistics.py
+++ statistics.py
@@ -1,23 +1,29 @@
-import sys, json
+import sys, json, glob
 
 with open("database/streamdata.json", "r") as f:
     streamData = json.load(f)
 channelIds = streamData.keys()
 args = sys.argv
 
-# get livePointStatus
+# ライブポイント状況
 if "livePointStatus" == args[1]:
+    csvData = ""
     for channelId in channelIds:
         livePointStatus = streamData[channelId]["livePointStatus"]
-        print(",".join(map(str, list(livePointStatus.values()))))
+        csvData += ",".join(map(str, list(livePointStatus.values()))) + "\n"
+    with open('livePointStatus.csv', 'w', encoding='utf-8') as f:
+        f.write(csvData)
+    print("complete!")
 
 
-# get iconUpdateCount
+# アイコンアップデート状況
 if "iconUpdateCount" == args[1]:
     for channelId in channelIds:
         print(streamData[channelId]["iconUpdateCount"])
+    print("complete!")
 
-# get games
+
+# ゲームランキング
 if "games" == args[1]:
     gameLst = {}
     for channelId in channelIds:
@@ -27,10 +33,40 @@ if "games" == args[1]:
                 gameLst[game["product"]] += 1
             else:
                 gameLst[game["product"]] = 1
+    csvData = ""
     for product in gameLst.keys():
-        print(f"\"{product}\",{gameLst[product]}")
+        csvData += f"\"{product}\",{gameLst[product]}\n"
 
-# get collab
+    with open('games.csv', 'w', encoding='utf-8') as f:
+        f.write(csvData)
+    print("complete!")
+
+
+# コラボ人数状況
 if "collab" == args[1]:
     for channelId in channelIds:
         print(len(streamData[channelId]["collab"]))
+    print("complete!")
+
+
+# 動画タイトルの【】内に入れている単語ランキング
+if "videoTag" == args[1]:
+    streamingsPath = glob.glob('log/streaming/*')
+    videoTags = {}
+    for streamingPath in streamingsPath:
+        with open(streamingPath, 'r') as f:
+            data = json.load(f)
+            for d in data:
+                for detail in d['details']:
+                    videoTag = detail['videoTag']
+                    if videoTag in videoTags.keys():
+                        videoTags[videoTag] += 1
+                    else:
+                        videoTags[videoTag] = 1
+    csvData = ""
+    for videoTag in videoTags.keys():
+        csvData += f"\"{videoTag}\",{videoTags[videoTag]}\n"
+
+    with open('videoTag_ranking.csv', 'w', encoding='utf-8') as f:
+        f.write(csvData)
+    print("complete!")
