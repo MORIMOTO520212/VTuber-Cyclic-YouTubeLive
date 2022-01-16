1d1171de66ebeb61b112ca032e330b1ea7683760
moirmoto <kanten520212@gmail.com>
2022-01-16 10:34:43 +0900

    2022.01.16

---
+++ module/userId_to_channelId.py
@@ -0,0 +1,16 @@
+# userId_to_channelId.py
+# create: 2022.01.16
+# author: YUMA.morimoto
+# description: 
+
+import re, requests
+
+
+def convert_channelId(user_id):
+    'ユーザーIDからチャンネルIDに変換する。\
+    user_id = \'/c/SunadaZenon\''
+
+    resp = requests.get('https://www.youtube.com'+user_id)
+    res = re.findall('\"browse_id\",\"value\":\".{24}', resp.text)[0]
+    channel_id = res[-24:]
+    return channel_id
