36563fa89bc376c6f71807667ac970c4b0996d49
MorimotoYuma <kanten520212@gmail.com>
2022-01-23 17:23:36 +0900

    2022/01/23

--- statistics.py
+++ statistics.py
@@ -1,3 +1,13 @@
+# データ分析ツール
+# 【オプション】
+# livePointStatus   - ライブポイント状況
+# iconUpdateCount   - アイコンアップデート状況
+# games             - ゲームランキング
+# collab            - コラボ人数状況
+# videoTag          - 動画タイトルの【】内に入れている単語ランキング
+# users             - 曜日ごとの配信者数ランキング
+# timeUserNum       - 時間帯別視聴者数推移
+
 import sys, json, glob, datetime
 
 with open("database/streamdata.json", "r") as f:
@@ -99,3 +109,78 @@ if "users" == args[1]:
 
     with open('stream_ranking.csv', 'w', encoding='utf-8') as f:
         f.write(csvData)
+
+
+# 時間帯別視聴者数推移
+if "timeUserNum" == args[1]:
+    streamings = glob.glob('log/streaming/*')
+
+    data_hour = {
+        "00": [], "01": [], "02": [],
+        "03": [], "04": [], "05": [],
+        "06": [], "07": [], "08": [],
+        "09": [], "10": [], "11": [],
+        "12": [], "13": [], "14": [],
+        "15": [], "16": [], "17": [],
+        "18": [], "19": [], "20": [],
+        "21": [], "22": [], "23": [],
+    }
+
+    for streaming in streamings:
+        with open(streaming, 'r') as f:
+            data = json.load(f)
+
+        for d in data:
+            timestamp = d['timestamp']
+            dt = datetime.datetime.fromtimestamp(timestamp)
+            hour = dt.strftime('%H') # get day of week
+            stack = []
+
+            for detail in d['details']:
+                streamingNumber = detail['streamingNumber']
+                if "万人" in streamingNumber:
+                    streamingNumber = float(streamingNumber.replace('万人', '')) * 10000
+                elif "人" in streamingNumber:
+                    streamingNumber = int(streamingNumber.replace('人', ''))
+                stack.append(streamingNumber)
+            
+            data_hour[hour].extend(stack)
+
+    for dowKey in data_hour.keys():
+        streamingNumberAve = sum(data_hour[dowKey]) / len(data_hour[dowKey])
+        print(f"{dowKey},{streamingNumberAve}")
+
+
+# 曜日別視聴者数推移
+if "dayUserNum" == args[1]:
+    streamings = glob.glob('log/streaming/*')
+
+    data_day_of_week = {
+        "Mon": [], "Tue": [], "Wed": [],
+        "Thu": [], "Fri": [], "Sat": [],
+        "Sun": []
+    }
+
+    for streaming in streamings:
+        with open(streaming, 'r') as f:
+            data = json.load(f)
+
+        for d in data:
+            timestamp = d['timestamp']
+            dt = datetime.datetime.fromtimestamp(timestamp)
+            day_of_week = dt.strftime('%a') # get day of week
+            stack = []
+
+            for detail in d['details']:
+                streamingNumber = detail['streamingNumber']
+                if "万人" in streamingNumber:
+                    streamingNumber = float(streamingNumber.replace('万人', '')) * 10000
+                elif "人" in streamingNumber:
+                    streamingNumber = int(streamingNumber.replace('人', ''))
+                stack.append(streamingNumber)
+            
+            data_day_of_week[day_of_week].extend(stack)
+
+    for dowKey in data_day_of_week.keys():
+        streamingNumberAve = sum(data_day_of_week[dowKey]) / len(data_day_of_week[dowKey])
+        print(f"{dowKey},{streamingNumberAve}")
